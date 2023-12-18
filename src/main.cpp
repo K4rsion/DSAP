b5ae8df80af6bb449ae2fd6c04cecd0eb18b2ff9
Your Name <you@example.com>
2023-12-18 22:12:57 +0700

    Code refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,56 @@
 #include <Arduino.h>
-
 #include <SPIFFS.h>
 #include <Wire.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "ArduinoJson.h"
 #include "driver/adc.h"
 #include "driver/dac.h"
-
-#include <WiFi.h>
 #include "AsyncTCP.h"
-#include "ESPAsyncWebServer.h"
-
-#include "ArduinoJson.h"
 
 #define THRESHOLD 1550
 
-// Replace with your network credentials
 const char *ssid = "ESP";
 const char *password = "12345678";
 
-// Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-IPAddress IP(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
+const int bufferSize = 1024;
+uint16_t audioBuffer[bufferSize];
 
-const int micPin = 33;         // Microphone input pin
-const int bufferSize = 1024;   // Adjust the buffer size as needed
+uint16_t pitchShiftFactor = 10;
 
-int16_t audioBuffer[bufferSize];
-
-int pitchShiftFactor = 10;
-
-double changePitchLvl(double newPitch) {
+uint16_t changePitchLvl(uint16_t newPitch) {
     pitchShiftFactor = newPitch;
     return pitchShiftFactor;
 }
 
-void setup() {
-    Serial.begin(115200);
+// Configure the ADC
+void adc_conf() {
+    adc1_config_width(ADC_WIDTH_MAX);
+    adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
+}
 
-    // Initialize the DAC
-    dac_output_enable(DAC_CHANNEL_1);  // Enable DAC channel 1
+// Configure the DAC
+void dac_conf() {
+    dac_output_enable(DAC_CHANNEL_1);
+}
 
-    // Configure the ADC for the microphone input
-    adc1_config_width(ADC_WIDTH_BIT_12);   // 12-bit resolution
-    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_0db);
-
-    // Initialize SPIFFS
-    if (!SPIFFS.begin()) {
-        return;
-    }
-
-    // Connect to Wi-Fi
+// Configure the Wi-Fi and server
+void server_start() {
+    IPAddress ip(192, 168, 1, 1);
+    IPAddress gateway(192, 168, 1, 1);
+    IPAddress subnet(255, 255, 255, 0);
     WiFi.softAP(ssid, password);
     delay(500);
-    WiFi.softAPConfig(IP, gateway, subnet);
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("IP: ");
-    Serial.println(IP);
+    WiFi.softAPConfig(ip, gateway, subnet);
+    IPAddress ipAddress = WiFi.softAPIP();
+    Serial.print("ip: ");
+    Serial.println(ipAddress);
+}
 
-    // Route for root / web page
+// Configure routes
+void router_init() {
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(SPIFFS, "/index.html");
     });
@@ -70,24 +61,33 @@ void setup() {
         if (request->url() == "/changePitchLvl") {
             DynamicJsonDocument doc(1024);
             deserializeJson(doc, data);
-            double valueNum = doc["value"];
+            uint16_t valueNum = doc["value"];
             changePitchLvl(valueNum);
         }
         request->send(200, "text/plain", "changed");
     });
+}
 
-    // Start server
+void setup() {
+    Serial.begin(115200);
+
+    // Initialize SPIFFS
+    if (!SPIFFS.begin()) {
+        return;
+    }
+
+    adc_conf();
+    dac_conf();
+    server_start();
+    router_init();
     server.begin();
 }
 
 void loop() {
     for (int i = 0; i < bufferSize; i++) {
-        // Read audio data from the microphone
-        audioBuffer[i] = analogRead(micPin);
+        audioBuffer[i] = adc1_get_raw(ADC1_CHANNEL_4);
         if (audioBuffer[i] <= THRESHOLD) {
-            // Apply pitch shift effect (lower the pitch)
             audioBuffer[i] = audioBuffer[i] / pitchShiftFactor;
-            // Send the audio data to the speaker
             dac_output_voltage(DAC_CHANNEL_1, audioBuffer[i]);
         }
     }
