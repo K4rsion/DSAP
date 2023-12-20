22b8e23c08ba7d1915d93c11e35cff20d10861e4
buljad <i.zhuravskii@g.nsu.ru>
2023-12-21 01:55:46 +0700

    Add frontend designed part with a slider

---
+++ script.js
@@ -0,0 +1,33 @@
+const numericInput = document.getElementById('pitch');
+const submitButton = document.getElementById('submitButton');
+
+submitButton.addEventListener('click', () => {
+    const numericValue = parseFloat(numericInput.value);
+    fetch('changePitchLvl', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ value: numericValue })
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                console.error('Request failed(frontend)');
+                return null;
+            }
+        })
+        .then(data => {
+            if (data !== null) {
+                console.log(data);
+            }
+        })
+});
+
+function outputUpdate(vol) {
+    var output = document.querySelector('#volume');
+    output.value = vol;
+    output.style.left = vol - 20 + 'px';
+}
