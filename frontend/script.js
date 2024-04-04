f830ddf907ac8eadc749c0e89d282494791471ec
buljad <i.zhuravskii@g.nsu.ru>
2024-04-04 11:14:52 +0700

    Refactored volume level slider

--- frontend/script.js
+++ frontend/script.js
@@ -1,36 +1,56 @@
 const numericInput = document.getElementById('pitch');
-const submitButton = document.getElementById('submitButton');
+const submitButton1 = document.getElementById('submitButton1');
 const toggleButton = document.getElementsById('toggleButton');
 
-submitButton.addEventListener('click', () => {
-    const numericValue = parseFloat(numericInput.value);
-    fetch('changePitchLvl', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ value: numericValue })
-    })
+// submitButton.addEventListener('click', () => {
+//     const numericValue = parseFloat(numericInput.value);
+//     fetch('changePitchLvl', {
+//         method: 'POST',
+//         headers: {
+//             'Accept': 'application/json',
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ value: numericValue })
+//     })
+//         .then(response => {
+//             if (response.ok) {
+//                 return response.json();
+//             } else {
+//                 console.error('Request failed(frontend)');
+//                 return null;
+//             }
+//         })
+//         .then(data => {
+//             if (data !== null) {
+//                 console.log(data);
+//             }
+//         })
+// });
+
+
+
+document.getElementById('submitButton1').addEventListener('click', function () {
+    // Получаем значение со слайдера
+    var sliderValue = document.getElementById('slider').value;
+
+    // Формируем URL с параметром значения слайдера
+    var url = 'http://192.168.1.1/changeVolume?volumeValue=' + sliderValue;
+
+    // Отправляем GET запрос
+    fetch(url)
         .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                console.error('Request failed(frontend)');
-                return null;
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
             }
+            return response.json();
         })
-        .then(data => {
-            if (data !== null) {
-                console.log(data);
-            }
-        })
+        .then(data => console.log(data))
+        .catch(error => console.error('Ошибка:', error));
 });
 
 function outputUpdate(vol) {
-    var output = document.querySelector('#volume');
-    output.value = vol;
-    output.style.left = vol - 20 + 'px';
+    var output = document.querySelector('#sliderValue');
+    output.innerHTML = vol;
 }
 
 toggleButton.addEventListener('click', () => {
