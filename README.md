6831b8ca1d1b8e992e17cebff4ac641c083cf8e6
Your Name <you@example.com>
2024-05-20 00:15:17 +0700

    add README, /clear and /state endpoints

--- README.md
+++ README.md
@@ -1,5 +1,60 @@
-# DSAP
+## **DSAP**
 
-### **Сборка:**
-- Встроенный ADC + микрофон (33 пин)
-- Внешний DAC (BCK: 14 пин; LCK = 26 пин; DATA = 27 пин)
+---
+
+### Overview
+This project aims to implement voice transformation using the ESP32 microcontroller. The ESP32 is utilized along with external ADCs and DACs to process audio input and output. The system supports volume control and three audio effects: pitch, delay, and distortion. It provides flexibility in terms of input and output devices, allowing the connection of microphones, speakers, or other audio devices via auxiliary (aux) ports. Additionally, a web server is hosted on the ESP32 to facilitate the configuration of effect parameters.
+
+### Features
+- Voice transformation using ESP32 microcontroller.
+- Utilization of external ADCs and DACs for audio processing.
+- Support for volume control.
+- Implementation of three audio effects:
+  - Pitch shifting.
+  - Delay effect.
+  - Distortion effect.
+- Flexibility in input and output device connection:
+  - Microphone input via aux.
+  - Output to speakers or other devices via aux.
+- Web server hosted on ESP32 for configuring effect parameters.
+
+### Hardware:
+
+#### ADC:
+| PCM1808 | ESP32
+|-------|---------------
+| DATA  | GPIO32
+| BCKL  | GPIO27
+| LRCK  | GPIO14
+| MCLK  | GPIO0
+
+#### DAC:
+| PCM5102A | ESP32
+|----------|---------------
+| SCK      | GND
+| BCK      | GPIO26
+| DIN      | GPIO25
+| LCK      | GPIO33
+
+
+### Library
+This project utilizes the [arduino-audio-tools](https://github.com/pschatzmann/arduino-audio-tools) library for audio processing tasks. The library provides essential tools and functionalities for implementing audio effects on microcontrollers.
+
+### Usage
+1. Adjust volume using the provided controls.
+2. Enable/disable audio effects (pitch, delay, distortion) as needed.
+3. Connect input devices (microphone) and output devices (speakers) via aux ports.
+4. Access the web server hosted on the ESP32 to configure effect parameters.
+5. Begin voice transformation by interacting with the system.
+
+### Contributions
+Contributions to this project are welcome! If you have suggestions for improvements or new features, feel free to submit a pull request.
+
+### Credits
+- Developed by [K4rsion](https://github.com/K4rsion), [buljad](https://github.com/buljad), [SSKaZZDom](https://github.com/SSKaZZDom)
+- Inspired by [mordv](https://github.com/mordv)
+
+### Contact
+For inquiries or support, please contact [Your Email].
+
+---
--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 #include "ESPAsyncWebServer.h"
 #include "AsyncTCP.h"
 
-#define ssid "DSAP"
-#define password "12345678"
+#define SSID "DSAP"
+#define PASSWORD "12345678"
 
 I2SStream in;
 I2SStream out;
@@ -18,21 +18,10 @@ AudioEffectStream effects(out);
 FormatConverterStream conv(effects);
 StreamCopy copier(conv, in);
 
-Boost boost(1.0);  // громкость
-PitchShift pitchShift(1.0, 1000);
+Boost boost(1.0);
+PitchShift pitchShift(1.0, 400);
 Distortion distortion(4990, 6500);
 Delay delayEffect(450, 0.5, 1.0, 44100);
-Tremolo tremolo(2000, 50, 44100);
-
-//delay:
-//duration = 100-450
-//depth = 0.1-1.5
-//feedback = 0.1-1.5 (>= 1.0 - жесткое нарастание звука)
-//
-//tremolo:
-//duration = 50-250
-//depthPercent = 0-100
-
 
 IPAddress ip(192, 168, 1, 1);
 IPAddress gateway(192, 168, 1, 1);
@@ -40,9 +29,43 @@ IPAddress subnet(255, 255, 255, 0);
 
 AsyncWebServer server(80);
 
-/*
- * Create routes and determine handle functions to each route.
- */
+void startServer();
+void configureI2SIn();
+void configureI2SOut();
+void router();
+
+void setup() {
+    SPIFFS.begin();
+    startServer();
+
+    configureI2SIn();
+    configureI2SOut();
+
+    pitchShift.setActive(false);
+    distortion.setActive(false);
+    delayEffect.setActive(false);
+
+    effects.addEffect(boost);
+    effects.addEffect(pitchShift);
+    effects.addEffect(distortion);
+    effects.addEffect(delayEffect);
+    effects.begin(info16);
+
+    conv.begin(info32, info16);
+}
+
+void loop() {
+    copier.copy();
+}
+
+void startServer() {
+    WiFi.softAP(SSID, PASSWORD);
+    delay(500);
+    WiFi.softAPConfig(ip, gateway, subnet);
+    server.begin();
+    router();
+}
+
 void router() {
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
         request->send(SPIFFS, "/index.html");
@@ -52,188 +75,85 @@ void router() {
         request->send(SPIFFS, "/script.js", "text/javascript");
     });
 
-
-    //"http://192.168.1.1/pitch?valueValue=1.0"
     server.on("/volume", HTTP_PUT, [](AsyncWebServerRequest *request) {
         if (request->hasParam("volumeValue")) {
-            String volumeValueStr = request->getParam("volumeValue")->value();
-            float volumeValue = volumeValueStr.toFloat();
+            float volumeValue = request->getParam("volumeValue")->value().toFloat();
             boost.setVolume(volumeValue);
         }
         request->send(200, "text/plain", "OK");
     });
 
+    server.on("/state", HTTP_GET, [](AsyncWebServerRequest *request) {
+        String response = "{";
+        response += "\"pitchEnabled\":" + String(pitchShift.active() ? 1 : 0) + ",";
+        response += "\"pitchShiftValue\":" + String(pitchShift.value()) + ",";
+        response += "\"distortionEnabled\":" + String(distortion.active() ? 1 : 0) + ",";
+        response += "\"clipThreshold\":" + String(distortion.clipThreashold()) + ",";
+        response += "\"maxInput\":" + String(distortion.maxInput()) + ",";
+        response += "\"delayEnabled\":" + String(delayEffect.active() ? 1 : 0) + ",";
+        response += "\"delayDuration\":" + String(delayEffect.getDuration()) + ",";
+        response += "\"delayDepth\":" + String(delayEffect.getDepth()) + ",";
+        response += "\"delayFeedback\":" + String(delayEffect.getFeedback()) + ",";
+        response += "\"boostVolume\":" + String(boost.volume());
+        response += "}";
 
-    server.on("/pitchOn", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (!pitchShift.active()) {
-            pitchShift.setActive(true);
-        }
-        request->send(200, "text/plain", "OK");
+        request->send(200, "application/json", response);
     });
 
-    server.on("/pitchOff", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (pitchShift.active()) {
-            pitchShift.setActive(false);
-        }
-        request->send(200, "text/plain", "OK");
-    });
-
-    //"http://192.168.1.1/pitch?shiftValue=1.0"
     server.on("/pitch", HTTP_PUT, [](AsyncWebServerRequest *request) {
+        if (request->hasParam("enabled")) {
+            pitchShift.setActive(request->getParam("enabled")->value().toInt() == 1);
+        }
         if (request->hasParam("shiftValue")) {
-            String shiftValueStr = request->getParam("shiftValue")->value();
-            float shiftValue = shiftValueStr.toFloat();
-            pitchShift.setValue(shiftValue);
-        }
-        request->send(200, "text/plain", "OK");
-    });
-
-
-    server.on("/distortionOn", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (!distortion.active()) {
-            distortion.setActive(true);
-        }
-        request->send(200, "text/plain", "OK");
-    });
-
-    server.on("/distortionOff", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (distortion.active()) {
-            distortion.setActive(false);
+            pitchShift.setValue(request->getParam("shiftValue")->value().toFloat());
         }
         request->send(200, "text/plain", "OK");
     });
 
     server.on("/distortion", HTTP_PUT, [](AsyncWebServerRequest *request) {
+        if (request->hasParam("enabled")) {
+            distortion.setActive(request->getParam("enabled")->value().toInt() == 1);
+        }
         if (request->hasParam("clipThreshold")) {
-            String clipThresholdStr = request->getParam("clipThreshold")->value();
-            int16_t clipThreshold = clipThresholdStr.toInt();
-            distortion.setClipThreashold(clipThreshold);
+            distortion.setClipThreashold(request->getParam("clipThreshold")->value().toInt());
         }
-
         if (request->hasParam("maxInput")) {
-            String maxInputStr = request->getParam("maxInput")->value();
-            int16_t maxInput = maxInputStr.toInt();
-            distortion.setMaxInput(maxInput);
-        }
-
-        request->send(200, "text/plain", "OK");
-    });
-
-
-    server.on("/delayOn", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (!delayEffect.active()) {
-            delayEffect.setActive(true);
-        }
-        request->send(200, "text/plain", "OK");
-    });
-
-    server.on("/delayOff", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (delayEffect.active()) {
-            delayEffect.setActive(false);
+            distortion.setMaxInput(request->getParam("maxInput")->value().toInt());
         }
         request->send(200, "text/plain", "OK");
     });
 
     server.on("/delay", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (request->hasParam("duration")) {
-            String durationStr = request->getParam("duration")->value();
-            uint16_t duration = durationStr.toInt();
-            delayEffect.setDuration(duration);
+        if (request->hasParam("enabled")) {
+            delayEffect.setActive(request->getParam("enabled")->value().toInt() == 1);
         }
-
+        if (request->hasParam("duration")) {
+            delayEffect.setDuration(request->getParam("duration")->value().toInt());
+        }
         if (request->hasParam("depth")) {
-            String depthStr = request->getParam("depth")->value();
-            float depth = depthStr.toFloat();
-            delayEffect.setDepth(depth);
+            delayEffect.setDepth(request->getParam("depth")->value().toFloat());
         }
-
         if (request->hasParam("feedbackAmount")) {
-            String feedbackAmountStr = request->getParam("feedbackAmount")->value();
-            float feedbackAmount = feedbackAmountStr.toFloat();
-            delayEffect.setFeedback(feedbackAmount);
-        }
-
-        request->send(200, "text/plain", "OK");
-    });
-
-
-
-    server.on("/tremoloOn", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (!tremolo.active()) {
-            tremolo.setActive(true);
+            delayEffect.setFeedback(request->getParam("feedbackAmount")->value().toFloat());
         }
         request->send(200, "text/plain", "OK");
     });
 
-    server.on("/tremoloOff", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (tremolo.active()) {
-            tremolo.setActive(false);
-        }
-        request->send(200, "text/plain", "OK");
-    });
-
-    server.on("/tremolo", HTTP_PUT, [](AsyncWebServerRequest *request) {
-        if (request->hasParam("duration")) {
-            String durationStr = request->getParam("duration")->value();
-            int16_t duration = durationStr.toInt();
-            tremolo.setDuration(duration);
-        }
-
-        if (request->hasParam("depthPercent")) {
-            String depthStr = request->getParam("depthPercent")->value();
-            uint8_t depth = depthStr.toInt();
-            tremolo.setDepth(depth);
-        }
-
-        request->send(200, "text/plain", "OK");
-    });
-
-
-
-    //clear all effects
     server.on("/clear", HTTP_PUT, [](AsyncWebServerRequest *request) {
-
         pitchShift.setValue(1.0);
         pitchShift.setActive(false);
-
         distortion.setClipThreashold(4990);
         distortion.setMaxInput(6500);
         distortion.setActive(false);
-
         delayEffect.setDuration(450);
         delayEffect.setDepth(0.5);
         delayEffect.setFeedback(1.0);
         delayEffect.setActive(false);
-
-        tremolo.setDuration(2000);
-        tremolo.setDepth(50);
-        tremolo.setActive(false);
-
         request->send(200, "text/plain", "OK");
     });
 }
 
-
-void start_server() {
-    SPIFFS.begin();
-    router();
-    WiFi.softAP(ssid, password);
-    delay(500);
-    WiFi.softAPConfig(ip, gateway, subnet);
-    IPAddress ipAddress = WiFi.softAPIP();
-    Serial.print("Server IP: ");
-    Serial.println(ipAddress);
-    server.begin();
-}
-
-
-void setup() {
-//    Serial.begin(115200);
-//    AudioLogger::instance().begin(Serial, AudioLogger::Info);
-
-    start_server();
-
-    Serial.println("starting ADC...");
+void configureI2SIn() {
     auto config_in = in.defaultConfig(RX_MODE);
     config_in.copyFrom(info32);
     config_in.i2s_format = I2S_STD_FORMAT;
@@ -245,9 +165,9 @@ void setup() {
     config_in.pin_mck = 0;
     config_in.fixed_mclk = 256 * info32.sample_rate;
     in.begin(config_in);
-    Serial.println("ADC started...");
+}
 
-    Serial.println("Starting DAC...");
+void configureI2SOut() {
     auto config_out = out.defaultConfig(TX_MODE);
     config_out.copyFrom(info16);
     config_out.i2s_format = I2S_STD_FORMAT;
@@ -257,27 +177,4 @@ void setup() {
     config_out.pin_data = 25;
     config_out.pin_ws = 33;
     out.begin(config_out);
-    Serial.println("DAC started...");
-
-    // Эффекты
-    pitchShift.setActive(false);
-    distortion.setActive(false);
-    delayEffect.setActive(false);
-    tremolo.setActive(false);
-
-
-    effects.addEffect(boost);
-    effects.addEffect(pitchShift);
-    effects.addEffect(distortion);
-    effects.addEffect(delayEffect);
-    effects.addEffect(tremolo);
-    effects.begin(info16);
-
-    Serial.println("Starting conv...");
-    conv.begin(info32, info16);
-    Serial.println("Conv started...");
-}
-
-void loop() {
-    copier.copy();
 }
